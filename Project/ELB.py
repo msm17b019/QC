0d08c9e41aa2fda21d6781559f85d4e0d178103b
msm17b019 <sujeetkumariiit@gmail.com>
2023-05-29 14:54:57 +0530

    added load balancer rule

--- Project/ELB.py
+++ Project/ELB.py
@@ -7,7 +7,7 @@ class Elb:
         """
         self.elbv2_client = elbv2_client
 
-    def create_elb(self, name: str, pub_sub: list, tags: list, elb_sg: str):
+    def create_elb(self, name: str, pub_sub: list, tags: list, elb_sg: str, vpc_id: str):
         """This method creates application load balancer.
 
         Args:
@@ -17,7 +17,7 @@ class Elb:
             elb_sg (str): Security group ID.
         """
         if self.check_elb(name):
-            self.elbv2_client.create_load_balancer(
+            response1 = self.elbv2_client.create_load_balancer(
                 Name=name,
                 Subnets=pub_sub,
                 SecurityGroups=[
@@ -27,6 +27,43 @@ class Elb:
                 Tags=tags,
                 Type='application',
                 IpAddressType='ipv4',
+            )
+            self.elb_arn = response1['LoadBalancers'][0]['LoadBalancerArn']
+            self.elb_dns_name = response1['LoadBalancers'][0]['DNSName']
+
+            response2 = self.elbv2_client.create_listener(
+                LoadBalancerArn=self.elb_arn,
+                Protocol='HTTP',
+                Port=80,
+            )
+            self.listener_arn = response2['Listeners'][0]['ListenerArn']
+
+            response3 = self.elbv2_client.create_target_group(
+                Name='QubeTG',
+                Protocol='HTTP',
+                Port=80,
+                VpcId=vpc_id,
+                TargetType='instance',
+                Tags=tags
+            )
+            self.target_group_arn = response3['TargetGroups'][0]['TargetGroupArn']
+
+            response4 = self.elbv2_client.create_rule(
+                ListenerArn=self.listener_arn,
+                Conditions=[
+                    {
+                        'Field': 'path-pattern',
+                        'Values': [
+                            f'{self.elb_dns_name}/worldsogood'
+                        ]
+                    }
+                ],
+                Actions=[
+                    {
+                        'Type': 'forward',
+                        'TargetGroupArn': self.target_group_arn
+                    }
+                ]
             )
 
     def check_elb(self, name) -> bool:
